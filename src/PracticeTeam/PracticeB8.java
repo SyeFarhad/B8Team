5890bff7279585c147d1d7416630ae442990ae62
Reyhana.Hussaini <Reyhanehg.hussaini@gmail.com>
2023-01-14 18:30:43 -0500

    werr

--- src/PracticeTeam/PracticeB8.java
+++ src/PracticeTeam/PracticeB8.java
@@ -1,9 +1,7 @@
 package PracticeTeam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class PracticeB8 {
 	public static void main(String[] args) {
@@ -18,21 +16,18 @@ public class PracticeB8 {
 			
 		}
 		System.out.println("************************************");
-			String[] names = {"Abdul", "Farid", "Ebad", "Farhad", "Milad", "Nargis"};
+			String[] names = {"Abdul", "Farid", "Ebad", "Farhad", "Milad", "Nargis" , "Reyahana"};
 			for (String name : names) {
 				System.out.println(name.toUpperCase());
 		}
-		    System.out.println("*******************************************");
 		List<String> str = new ArrayList<>();
 		str.add("فرهاد");
+		str.add("فرید");
 		str.add("نرگس");
+		str.add("ریحانه");
 		str.add("عبدل");
 		for (String name : str) {
 		    System.out.println(name);
-		
-		    
 		}
-		
-		
 	}
 }
