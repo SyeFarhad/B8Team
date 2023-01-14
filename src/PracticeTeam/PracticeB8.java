1b3a5f9398ae1a25af01166335d5f7c9598d8310
Ebad <zalmaikapis@gmail.com>
2023-01-14 17:55:07 -0500

    New team created.

--- src/PracticeTeam/PracticeB8.java
+++ src/PracticeTeam/PracticeB8.java
@@ -1,6 +1,23 @@
 package PracticeTeam;
 
 public class PracticeB8 {
+	public static void main(String[] args) {
+		
+		
 
-	int age = 45;
+		int age = 44;
+		if (age == 45) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+			
+		}
+		System.out.println("************************************");
+			String[] names = {"Abdul", "Farid", "Ebad", "Farhad", "Milad", "Nargis"};
+			for (String name : names) {
+				System.out.println(name.toUpperCase());
+			
+
+		}
+	}
 }
