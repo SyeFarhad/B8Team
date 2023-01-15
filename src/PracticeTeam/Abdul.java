51951fbe81fa59f138a277f5ee8e71f022054552
Abdul Qadir <Abdul Qadir@YOGA-C930-ABQ.hsd1.tn.comcast.net>
2023-01-14 22:39:30 -0500

    hi

--- src/PracticeTeam/Abdul.java
+++ src/PracticeTeam/Abdul.java
@@ -13,22 +13,22 @@ public class Abdul {
 		city.add("New York City");
 		city.add("Virginia");
 
-		for (String str : city) { // Enhanced for loop
+		for (String str : city) {                   // Enhanced for loop
 			System.out.println(str);
 		}
 		System.out.println("+++++++++++++++++++");
-		for (int i = 0; i < city.size(); i++) { // regular for loop
+		for (int i = 0; i < city.size(); i++) {     // regular for loop
 			System.out.println(city);
 		}
 		System.out.println("+++++++++++++++++++");
-		int j = 0; // while loop
+		int j = 0;                                  // while loop
 		while (j < city.size()) {
 			System.out.println(city.get(j));
 
 		}
 		System.out.println("+++++++++++++++++++");
 
-		int k = 0; // do- while loop
+		int k = 0;                                 // do- while loop
 		do {
 			System.out.println(city.get(k));
 			k++;
