087c7365cdf0bc671a5db9085df275d421e90145
Abdul Qadir <Abdul Qadir@YOGA-C930-ABQ.hsd1.tn.comcast.net>
2023-01-14 22:35:46 -0500

    Merge branch 'master' of https://github.com/SyeFarhad/B8Team.git

--- src/PracticeTeam/Abdul.java
+++ src/PracticeTeam/Abdul.java
@@ -5,30 +5,32 @@ import java.util.ArrayList;
 public class Abdul {
 
 	public static void main(String[] args) {
-  ArrayList<String> city = new ArrayList<>();
-  
-  city.add("Knoxville");
-  city.add("Nashville");
-  city.add("Ashville");
-  city.add("New York City");
-  
-  
-  for (String str : city) {      // Enhanced for loop
-	System.out.println(str);
-}
-      for (int i = 0; i < city.size(); i++) {       // regular for loop
-		System.out.println(city);
+		ArrayList<String> city = new ArrayList<>();
+
+		city.add("Knoxville");
+		city.add("Nashville");
+		city.add("Ashville");
+		city.add("New York City");
+
+		for (String str : city) { // Enhanced for loop
+			System.out.println(str);
+		}
+		System.out.println("+++++++++++++++++++");
+		for (int i = 0; i < city.size(); i++) { // regular for loop
+			System.out.println(city);
+		}
+		System.out.println("+++++++++++++++++++");
+		int j = 0; // while loop
+		while (j < city.size()) {
+			System.out.println(city.get(j));
+
+		}
+		System.out.println("+++++++++++++++++++");
+
+		int k = 0; // do- while loop
+		do {
+			System.out.println(city.get(k));
+			k++;
+		} while (k < city.size());
 	}
-      
-      int j = 0;                 // while loop
-      while (j < city.size()) {
-		System.out.println(city.get(j));
-		
-	}
-      int k = 0;                 // do- while loop
-      do {
-		System.out.println(city.get(k));
-		k++;
-	} while (k < city.size());
-}
 }
