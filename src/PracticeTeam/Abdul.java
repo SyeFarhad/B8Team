f9eca9d0ad897b21528ae521236e7bf5d5b61278
Abdul Qadir <Abdul Qadir@YOGA-C930-ABQ.hsd1.tn.comcast.net>
2023-01-14 22:14:52 -0500

    commit

---
+++ src/PracticeTeam/Abdul.java
@@ -0,0 +1,14 @@
+package PracticeTeam;
+
+public class Abdul {
+
+	public static void main(String[] args) {
+   int[] num = {-50, 130, 12345, 0, 500};
+   
+   int[] num2 =new int[] {-50, 120, 12345, 500};
+   int[] num3 = new int[5];
+   
+   System.out.println(num[3]);
+	}
+
+}
