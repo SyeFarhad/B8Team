b53d009c9f802e04445ce8ef171fe0998a1a5943
Ebad <zalmaikapis@gmail.com>
2023-01-14 21:34:09 -0500

    delete everything

--- src/PracticeTeam/PracticeB8.java
+++ src/PracticeTeam/PracticeB8.java
@@ -1,38 +1,12 @@
 package PracticeTeam;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class PracticeB8 {
-	public static void main(String[] args) {
+	
 		
 		
 
-		int age = 44;
-		if (age == 45) {
-			System.out.println("true");
-		}else {
-			System.out.println("false");
 			
 		}
-		System.out.println("************************************");
-			String[] names = {"Abdul", "Farid", "Ebad", "Farhad", "Milad", "Nargis" , "Reyahana"};
-			for (String name : names) {
-				System.out.println(name.toUpperCase());
-		}
-		List<String> str = new ArrayList<>();
-		str.add("فرهاد");
-		str.add("فرید");
-		str.add("نرگس");
-		str.add("ریحانه");
-		str.add("عبدل");
-		for (String name : str) {
-		    System.out.println(name);
-		}
-			int time = 8;
-			if ( time < 8)
-			System.out.println (" give me double coffee + redbul") ;
-			else
-			System.out. println(" work to exit ticket") ;
-	}
-}
+
